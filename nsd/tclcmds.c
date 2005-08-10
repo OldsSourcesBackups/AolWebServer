9b750eecdca9c88474bc8e218c847f5a043fca88
jgdavidson <jgdavidson>
2005-08-10 20:06:03 +0000

    Fixed typo in ns_adp_include proc.

--- nsd/tclcmds.c
+++ nsd/tclcmds.c
@@ -429,7 +429,7 @@ static char *initScript = \
 	    if [catch {eval _ns_adp_include $args} errMsg] {\n\
 		return -code error $errMsg\n\
 	    }\n\
-	    return -code -ok\n\
+	    return -code ok\n\
 	}";
 
 
