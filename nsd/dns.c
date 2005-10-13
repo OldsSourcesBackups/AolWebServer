ec935b7674e98648089057ee5c813d33d980866b
shmooved <shmooved>
2005-10-13 14:16:59 +0000

    Fixed deadlock in GetAddr().

--- nsd/dns.c
+++ nsd/dns.c
@@ -378,12 +378,10 @@ GetAddr(Ns_DString *dsPtr, char *host)
     if (result != 0) { 
 	DnsLogError("gethostbyname_r", h_errnop);
     } else {
-        ptr = (struct in_addr *) he.h_addr_list[i];
-        while (ptr != NULL) {
+        while ((ptr = (struct in_addr *) he.h_addr_list[i++]) != NULL) {
             ia.s_addr = ptr->s_addr;
             Tcl_DStringAppendElement(dsPtr, ns_inet_ntoa(ia));
             status = NS_TRUE;
-            ptr = (struct in_addr *) he.h_addr_list[i++];
         }
     }
     return status;
@@ -413,8 +411,7 @@ GetAddr(Ns_DString *dsPtr, char *host)
     if (he == NULL) {
 	DnsLogError("gethostbyname", h_errno);
     } else {
-        ptr = (struct in_addr *) he.h_addr_list[i];
-        while (ptr != NULL) {
+        while ((ptr = (struct in_addr *) he.h_addr_list[i++]) != NULL) {
             ia.s_addr = ptr->s_addr;
             Tcl_DStringAppendElement(dsPtr, ns_inet_ntoa(ia));
             status = NS_TRUE;
